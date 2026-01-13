7a601badced1bc21a9c5de5fbbf227fb07b06c9c
masaiff210880 <surajkumar231501@gmail.com>
2026-01-13 21:17:10 +0530

    custome component create

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Login } from './login/login';
+import { Register } from './register/register';
 // import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Login],
+  imports: [Login,Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
