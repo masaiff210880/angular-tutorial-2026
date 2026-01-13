e7e8e602924ffcea95605f5e9d4244a20cc84119
masaiff210880 <surajkumar231501@gmail.com>
2026-01-13 21:00:21 +0530

    new component create

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+// import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-tutorial-1');
+  name = 'Angular';
+  dec = signal('This is a declaration signal');
 }
